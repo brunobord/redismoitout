4ef36981015ef0fd384917d7a092042040495c72
Bruno Bord <bruno@jehaisleprintemps.net>
2012-08-08 23:53:36 +0200

    a bit of comment to document

--- redismoitout.rb
+++ redismoitout.rb
@@ -6,11 +6,27 @@ $messages = []
 
 # Helpers
 class String
+    # checks if a string is composed of digits
     def is_numeric?
         !!Float(self) rescue false
     end
 end
 
+# Shortcut to assign a value to the key
+def set_key(key, value)
+    $redis.set key, value
+    redirect "/k/#{key}/"
+end
+
+# Shortcut to increment a value to the key
+def incr_key(key)
+    $redis.incr key
+    redirect "/k/#{key}/"
+end
+
+
+
+# Flash Message. Should be displayed only once and vanish from the session
 class FlashMessage
     attr_accessor :message, :level
     def initialize(message, level='success')
@@ -20,16 +36,7 @@ class FlashMessage
     end
 end
 
-def set_key(key, value)
-    $redis.set key, value
-    redirect "/k/#{key}/"
-end
-
-def incr_key(key)
-    $redis.incr key
-    redirect "/k/#{key}/"
-end
-
+# Will be executed before every request
 before do
     pass if request.path_info == "/login/"
     if not $redis.nil?
@@ -45,6 +52,7 @@ before do
     end
 end
 
+# Home page.
 get '/' do
     @search = "*" # Default value
     if not params[:search].nil? and params[:search] != ""
@@ -54,10 +62,12 @@ get '/' do
     erb :index
 end
 
+# Access to the login form
 get '/login/' do
     erb :login
 end
 
+# Process the login form - Handles authentication
 post '/login/' do
     $redis = Redis.new :host=>params[:host], :port=>params[:port]
     if params[:password] != ''
@@ -72,11 +82,13 @@ post '/login/' do
     redirect '/'
 end
 
+# Log out.
 get '/logout/' do
     $redis = nil
     redirect '/'
 end
 
+# Display key details (value, meta-information)
 get '/k/:key/' do |key|
     @key = key
     begin
@@ -92,39 +104,47 @@ get '/k/:key/' do |key|
     erb :detail
 end
 
+# Process 'SET' form
 post '/set/' do
     set_key params[:key], params[:value]
 end
 
+# Process the "DEL" button
 get '/del/:key/' do |key|
     $redis.del params[:key]
     $messages.push(FlashMessage.new "`#{params[:key]}` deleted", "error")
     redirect '/'
 end
 
+# INCR button
 get '/incr/:key/' do
     incr_key params[:key]
 end
 
+# INCR form (modal popup) processing
 post '/incr/' do
     incr_key params[:key]
 end
 
+# RPUSH form processing
 post '/rpush/' do
     $redis.rpush params[:key], params[:value]
     redirect "/k/#{params[:key]}/"
 end
 
+# LPUSH form processing
 post '/lpush/' do
     $redis.lpush params[:key], params[:value]
     redirect "/k/#{params[:key]}/"
 end
 
+# EXPIRE form (modal popup) processing
 post '/expire/' do
     $redis.expire params[:key], params[:ttl]
     redirect "/k/#{params[:key]}/"
 end
 
+# INFO page (server general information)
 get '/info/' do
     @info = $redis.info
     erb :info
